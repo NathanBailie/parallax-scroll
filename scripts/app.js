6a3ec06bebc690d4bfbaa8935fcf796347b678be
Nathan <antnefedov33@gmail.com>
2023-12-14 18:02:56 +0300

    added smooth scroll

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,12 @@
 "use strict"
 
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+
+if (ScrollTrigger.isTouch !== 1) {
+    ScrollSmoother.create({
+        wrapper: '.wrapper',
+        content: '.content',
+        smooth: 1.5,
+        effects: true
+    })
+}
