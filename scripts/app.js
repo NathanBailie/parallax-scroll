fceafbbeeb960619d6cd5e5f0d0387afedcb0b58
Nathan <antnefedov33@gmail.com>
2023-12-14 18:13:29 +0300

    first window disappearing animation added

--- scripts/app.js
+++ scripts/app.js
@@ -9,4 +9,14 @@ if (ScrollTrigger.isTouch !== 1) {
         smooth: 1.5,
         effects: true
     })
+
+    gsap.fromTo('.hero', { opacity: 1 }, {
+        opacity: 0,
+        scrollTrigger: {
+            trigger: '.hero',
+            start: 'center',
+            end: '900',
+            scrub: true
+        }
+    })
 }
